5872cd5cac10c5156d384217a999c6003874b2d1
Michael Sinnott <mesinnott@gmail.com>
2016-10-18 10:04:53 -0600

    ocmmit

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ let server = express()
 
 server.use(express.static(__dirname + "/public"))
 
+//CRUD APPLICATION
+let todos= [];
+function addTodo(todo){
+  todos.push(todo);
+}
+
+function removeTodo(index){
+  todos.splice(index, 1)
+}
+
+
+
+
+
 
 server.listen(8080, function(){
   console.log("The server is running on port 8080")
